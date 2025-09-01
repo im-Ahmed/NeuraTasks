a87b3de309cd23d228f44b1c4c8ca0c4cf8abc11
im-Ahmed <ahmedfarooqim3@gmail.com>
2025-09-01 13:27:10 +0500

    Update button text from "Login" to "Click me" in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Login</Button>
+        <Button>Click me</Button>
       </div>
     </>
   );
