72166c0a35e7c00f341213684c10acb6f4f604e4
im-Ahmed <ahmedfarooqim3@gmail.com>
2026-01-21 18:21:29 +0500

    feat: update tar package version, improve inbox header, and add new slices for board, log, and task

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         {/* protected routes */}
         <Route path="/dashboard" element={<Layout />}>
           <Route index element={<Dashboard />} key="dashboard" />
-          <Route path="inbox" element={<h1>inbox</h1>} key="inbox" />{" "}
+          <Route path="inbox" element={<h1>Inbox</h1>} key="inbox" />{" "}
           <Route path="calendar" element={<h1>Calendar Page</h1>} />
           <Route path="search" element={<h1>Search Page</h1>} />
           <Route path="settings" element={<h1>Settings Page</h1>} />
